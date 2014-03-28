6700f9abbfcaa5d13cb5596cfe1f48a8a41e9d4d
Gerardo-oficina <vasquezgerardojr@gmail.com>
2014-03-28 18:10:30 -0430

    Algunos cambios

--- surda/surda/settings.py
+++ surda/surda/settings.py
@@ -65,9 +65,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-VE'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Caracas'
 
 USE_I18N = True
 
